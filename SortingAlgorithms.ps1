de0e276b485668a0d153a09233582cafb1838554
valentinov <valentinvecsernik@gmail.com>
2022-11-25 09:17:11 +0100

    SwapHelper function got created

--- SortingAlgorithms.ps1
+++ SortingAlgorithms.ps1
@@ -1,21 +1,29 @@
-#Selection sort
-function SelectionSort($array, $numOfItems) {
+#Selection sort O(n^2)
+function SelectionShort($targetList, [Int]$numOfItems) {
 
     for ($i = 0; $i -lt $numOfItems; $i++) {
         $min = $i
         for ($j = $i + 1; $j -lt $numOfItems; $j++) {
-            if ($array[$min] -gt $array[$j]) {
+            if ($targetList[$min] -gt $targetList[$j]) {
                 $min = $j
             }
         }
-        $tmp = $array[$i]
-        $array[$i] = $array[$min]
-        $array[$min] = $tmp
+        
+        SwapHelper -target $targetList -a $i -b $min
     }
 }
 
-$list = (1..3 | ForEach-Object { Get-Random -Minimum 1 -Maximum 300 })
+#Helper functon to swap two elements in a list
+function SwapHelper($target, $a, $b) {
+    $tmp = $target[$a]
+    $target[$a] = $target[$b]
+    $target[$b] = $tmp
+}
 
-$n = $list.Count
+$randomList = [System.Collections.Generic.List[int]]::new()
 
-SelectionShort -array $list -numOfItems $n
+$randomList = (1..5 | ForEach-Object { Get-Random -Minimum 1 -Maximum 311 })
+
+$n = $randomList.Count
+
+SelectionShort -targetList $randomList -numOfItems $n
