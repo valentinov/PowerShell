c9818f600d021ba03718f70ef2594bc2500b51ba
valentinov <valentinvecsernik@gmail.com>
2022-11-25 12:18:13 +0100

    BubbleSort added

--- SortingAlgorithms.ps1
+++ SortingAlgorithms.ps1
@@ -1,5 +1,5 @@
 #Selection sort O(n^2)
-function SelectionShort($targetList, [Int]$numOfItems) {
+function SelectionSort($targetList, [Int]$numOfItems) {
 
     for ($i = 0; $i -lt $numOfItems; $i++) {
         $min = $i
@@ -8,12 +8,27 @@ function SelectionShort($targetList, [Int]$numOfItems) {
                 $min = $j
             }
         }
-        
         SwapHelper -target $targetList -a $i -b $min
     }
 }
 
-#Helper functon to swap two elements in a list
+#Bubble sort O(n^2)
+function BubbleSort($targetList, [Int]$numOfItems) {
+    $i = $numOfItems - 1
+    
+    while ($i -gt 0) {
+        $idx = 0
+        for ($j = 0; $j -lt $i; $j++) {
+            if ($targetList[$j] -gt $targetList[$j + 1]) {
+                SwapHelper -target $targetList -a $j -b ($j + 1)
+                $idx = $j
+            }
+        }
+        $i = $idx
+    }
+}
+
+#Helper function to swap two elements in a list
 function SwapHelper($target, $a, $b) {
     $tmp = $target[$a]
     $target[$a] = $target[$b]
