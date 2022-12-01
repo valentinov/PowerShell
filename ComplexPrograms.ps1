a1f23689364630be6157c59859ab16e53ac1e500
valentinov <valentinvecsernik@gmail.com>
2022-12-01 22:03:11 +0100

    Sort function added

--- ComplexPrograms.ps1
+++ ComplexPrograms.ps1
@@ -1,3 +1,40 @@
+#Sort the even (%2 == 0) elements to the beginning of the array without swap
+function Sort() {
+
+    Param([System.Collections.Generic.List[int]]$array, [Int]$numOfItems)
+    
+    $left = 0
+    $right = $numOfItems-1
+    $tmp = $array[0]
+    $pc = 0
+
+    while ($left -lt $right) {
+        while (($left -lt $right) -and ($array[$right] % 2 -ne 0)) {
+            $right--
+        }
+        if ($left -lt $right) {
+            $array[$left] = $array[$right]
+            $left++
+            while (($left -lt $right) -and ($array[$left] % 2 -eq 0)) {
+                $left++
+            }
+            if ($left -lt $right) {
+                $array[$right] = $array[$left]
+                $right--
+            }
+        }
+    }
+    $array[$left] = $tmp
+    if ($array[$left] % 2 -eq 0) {
+        $pc = $left
+    }
+    else {
+        $pc = $left - 1
+    }
+    
+    Return ($array, $pc)
+}
+
 #Intersection
 function Intersection () {
 
@@ -5,7 +42,7 @@ function Intersection () {
         [System.Collections.Generic.List[int]]$array_2, [Int]$numOfItems_2)
     
     $y = [System.Collections.Generic.List[int]]::new()
-    $db = 0
+    $pc = 0
 
     for ($i = 0; $i -lt $numOfItems_1; $i++) {
         $j = 0
@@ -15,11 +52,11 @@ function Intersection () {
 
         if ($j -le $numOfItems_2) {
             $y.Add($array_1[$i])
-            $db++
+            $pc++
         }
     }
     
-    Return($y, $db)
+    Return($y, $pc)
 }
 
 #Union
@@ -29,12 +66,12 @@ function Union() {
         [System.Collections.Generic.List[int]]$array_2, [Int]$numOfItems_2)
 
     $y = [System.Collections.Generic.List[int]]::new()
-    $db = 0
+    $pc = 0
 
     for ($i = 0; $i -lt $numOfItems_1; $i++) {
         $y.Add($array_1[$i])
     }
-    $db = $numOfItems_1
+    $pc = $numOfItems_1
 
     for ($j = 0; $j -lt $numOfItems_2; $j++) {
         $i = 0
@@ -43,9 +80,9 @@ function Union() {
         }
         if ($i -gt $numOfItems_1) {
             $y.Add($array_2[$j])
-            $db++
+            $pc++
         }
     }
     
-    Return ($y, $db)
+    Return ($y, $pc)
 }
