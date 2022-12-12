64325a5651b349eab37d57d30943abb188ccd694
valentinov <valentinvecsernik@gmail.com>
2022-12-12 11:21:46 +0100

    method calls added

--- SortingAlgorithms.ps1
+++ SortingAlgorithms.ps1
@@ -3,24 +3,27 @@ function SelectionSort($targetList, [Int]$numOfItems) {
 
     for ($i = 0; $i -lt $numOfItems; $i++) {
         $min = $i
+
         for ($j = $i + 1; $j -lt $numOfItems; $j++) {
             if ($targetList[$min] -gt $targetList[$j]) {
                 $min = $j
             }
         }
-        SwapHelper -target $targetList -a $i -b $min
+        SwapHelper -list $targetList -a $i -b $min
     }
 }
 
 #Bubble sort O(n^2)
 function BubbleSort($targetList, [Int]$numOfItems) {
+
     $i = $numOfItems - 1
-    
+
     while ($i -gt 0) {
         $idx = 0
+
         for ($j = 0; $j -lt $i; $j++) {
             if ($targetList[$j] -gt $targetList[$j + 1]) {
-                SwapHelper -target $targetList -a $j -b ($j + 1)
+                SwapHelper -list $targetList -a $j -b ($j + 1)
                 $idx = $j
             }
         }
@@ -39,22 +42,26 @@ function InsertionSort($targetList, [Int]$numOfItems) {
             $targetList[$j + 1] = $targetList[$j]
             $j = $j - 1
         }
-        
+
         $targetList[$j + 1] = $helper
     }
 }
 
 #Helper function to swap two elements in a list
-function SwapHelper($target, $a, $b) {
-    $tmp = $target[$a]
-    $target[$a] = $target[$b]
-    $target[$b] = $tmp
+function SwapHelper($list, $a, $b) {
+    $tmp = $list[$a]
+    $list[$a] = $list[$b]
+    $list[$b] = $tmp
 }
 
 $randomList = [System.Collections.Generic.List[int]]::new()
-
 $randomList = (1..5 | ForEach-Object { Get-Random -Minimum 1 -Maximum 311 })
 
+Write-Host($randomList)
 $n = $randomList.Count
 
-SelectionShort -targetList $randomList -numOfItems $n
+#SelectionSort -targetList $randomList -numOfItems $n
+
+#BubbleSort -targetList $randomList -numOfItems $n
+
+#InsertionSort -targetList $randomList -numOfItems $n
