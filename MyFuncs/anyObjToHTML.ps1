0c3e472c77cfa1d04eeb73ec055952ba9a21eab2
valentinov <valentinvecsernik@gmail.com>
2023-08-14 16:00:47 +0200

    CheckNColorTableData function created

--- MyFuncs/anyObjToHTML.ps1
+++ MyFuncs/anyObjToHTML.ps1
@@ -1,35 +1,3 @@
-<#
-.SYNOPSIS
-Converts any PowerShell objects into an HTML table with customizable formatting and coloring.
-
-.DESCRIPTION
-This function takes a PowerShell object as input and generates an HTML table that represents the object's data. The table can include custom header and footer content, and cell coloring based on specific conditions.
-
-.PARAMETER InputObject
-The PowerShell object to be converted into an HTML table. This object's properties will be used as columns in the table.
-
-.PARAMETER Header
-The header text to be displayed at the top of the generated HTML table.
-
-.PARAMETER Footer
-Optional. The footer text to be displayed at the bottom of the generated HTML table.
-
-.NOTES
-File Name      : anyObjToHTML.ps1
-Author         : Valentin Vecsernik
-Prerequisite   : PowerShell V2
-
-.EXAMPLE
-$sampleData = @(
-    [PSCustomObject]@{ Server = "app001"; Pool = "A"; Endpoint = "http://app001/healthcheck.php"; Status = 200 }
-    [PSCustomObject]@{ Server = "app002"; Pool = "B"; Endpoint = "http://app002/healthcheck.php"; Status = 503 }
-)
-$header = "User Information"
-$footer = "Total users: $($sampleData.Count)"
-
-$outputHtml = AnyObjectToHTMLTable -InputObject $sampleData -Header $header -Footer $footer
-$outputHtml | Out-File -FilePath "table.html"
-#>
 function AnyObjectToHTMLTable() {
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
@@ -72,23 +40,8 @@ function AnyObjectToHTMLTable() {
                 $value = $obj.$p
 
                 # Change the background color to green or red in some special cases
-                $valueStr = $value.ToString()
-                # green - ok
-                if ($valueStr.toLower() -in "200", "started", "online") { 
-                    $html += -join ("<td bgcolor=", '"LightGreen"', ">", $valueStr, "</td>")
-                }
-                # red - error
-                elseif ($valueStr.toLower() -in "404", "500", "503", "stopped", "down" -or [string]::IsNullOrEmpty($valueStr)) {
-                    # Handle empty value
-                    if ([string]::IsNullOrEmpty($valueStr)) {
-                        $valueStr = "NULL"
-                    }
-                    $html += -join ("<td bgcolor=", '"#f08080"', ">", $valueStr, "</td>")
-                }
-
-                else {
-                    $html += "<td>$valueStr</td>"
-                }
+                $valueStr = $value.ToString().ToLower()
+                $html += CheckNColorTableData -ValueSTR $valueStr
             }
             $html += "</tr>"
 
@@ -109,4 +62,36 @@ function AnyObjectToHTMLTable() {
         # Return the complete HTML string
         return $html
     }
+}
+
+<#
+Small helper for the HTML table creator function
+Based on ValueSTR string input generates the standard HTML Table Data <td> formatting and color
+Returns a $html string
+#>
+function CheckNColorTableData {
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $ValueSTR
+    )
+    $td = ""  # Initialize the td string
+    # green - ok
+    if ($ValueSTR -in "200", "started", "online") { 
+        $td += -join ("<td bgcolor=", '"LightGreen"', ">", $ValueSTR, "</td>")
+    }
+    # red - error
+    elseif ($ValueSTR -in "404", "500", "503", "stopped", "down" -or [string]::IsNullOrEmpty($ValueSTR)) {
+        # Handle empty value
+        if ([string]::IsNullOrEmpty($ValueSTR)) {
+            $ValueSTR = "NULL"
+        }
+        $td += -join ("<td bgcolor=", '"#f08080"', ">", $ValueSTR, "</td>")
+    }
+    # any other cases - no special formatting
+    else {
+        $td += "<td>$ValueSTR</td>"
+    }
+
+    return $td
 }
