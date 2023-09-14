ae622de3cb3875ec9d49b70a112bad87d12aa636
valentinov <valentinvecsernik@gmail.com>
2023-09-14 14:53:35 +0200

    Update houseKeeping.ps1

--- MyFuncs/houseKeeping.ps1
+++ MyFuncs/houseKeeping.ps1
@@ -1,3 +1,38 @@
+<#
+.SYNOPSIS
+    Performs housekeeping tasks by deleting log files older than a specified number of days in a given folder.
+
+.DESCRIPTION
+    The HouseKeeping function is used to automate the cleanup of log files in a specified folder. 
+    It checks if the folder exists, identifies log files that are older than a specified threshold,
+    and deletes them. This function provides options to customize the folder, retention period, 
+    and file extension for log files.
+
+.PARAMETER LogFolderPath
+    The folder path where log files are located.
+
+.PARAMETER DaysToDelete
+    The number of days to retain log files. Log files older than this number of days will be deleted.
+    (Default: 3)
+
+.PARAMETER FileExtension
+    The file extension of log files to be deleted. Specify the extension as a wildcard pattern.
+    (Default: "*.log")
+
+.NOTES
+    File deletions are performed using the `Remove-Item` cmdlet with the `-Force` parameter to ensure
+    that files are deleted even if they are read-only. Any errors encountered during deletion are
+    caught and reported.
+
+.EXAMPLE
+    HouseKeeping -LogFolderPath "C:\Logs" -DaysToDelete 7 -FileExtension "*.txt"
+    This example will clean up text log files in the "C:\Logs" folder that are older than 7 days.
+
+.EXAMPLE
+    HouseKeeping -LogFolderPath "D:\AppLogs"
+    This example will use default settings to clean up log files in the "D:\AppLogs" folder older than 3 days.
+
+#>
 function HouseKeeping {
     param (
         [string]$LogFolderPath,
@@ -30,6 +65,3 @@ function HouseKeeping {
         Write-Host "No files to delete."
     }
 }
-
-# Example usage:
-# HouseKeeping -LogFolderPath "C:\Logs" -DaysToDelete 7 -FileExtension "*.log"
