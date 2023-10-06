d92c9af504e423165720777765863d8e57ece2ae
valentinov <valentinvecsernik@gmail.com>
2023-10-06 15:30:26 +0200

    Create testPSSessionConfiguration.ps1

---
+++ MyFuncs/testPSSessionConfiguration.ps1
@@ -0,0 +1,44 @@
+function Test-PSSessionConfiguration {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$ConfigurationName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$ServerName
+    )
+
+    try {
+        $session = New-PSSession -ComputerName $ServerName -ErrorAction Stop
+
+        $scriptBlock = {
+            param ($ConfigurationName)
+
+            Get-PSSessionConfiguration -Name $ConfigurationName -ErrorAction SilentlyContinue
+        }
+
+        # Check if the custom configuration exists on the remote computer
+        $sessionConfig = Invoke-Command -Session $session -ScriptBlock $scriptBlock -ArgumentList $ConfigurationName
+
+        if ($null -ne $sessionConfig) {
+            Write-Host "OK! Custom configuration [$ConfigurationName] exists on [$ServerName] node."
+            return $sessionConfig
+        }
+        else {
+            Write-Host "FAIL! Custom configuration [$ConfigurationName] does not exist on [$ServerName]. Must register it before running the patching."
+        }
+    }
+    catch {
+        Write-Host "ERROR! Failed to check custom configuration on [$ServerName]. Error: $($_.Exception.Message)"
+    }
+    finally {
+        if ($session.State -eq 'Opened') {
+            Remove-PSSession -Session $session
+        }
+    }
+}
+
+# Example usage:
+# $config = Test-PSSessionConfiguration -ConfigurationName "MyConfig" -ServerName "Server1"
+# if ($config -ne $null) {
+#     # Do something with the configuration
+# }
